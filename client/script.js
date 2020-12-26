12d51203ef178b7c54328a04f82dfa4018777a5d
RehamMahmoued <52599652+RehamMahmoued@users.noreply.github.com>
2020-12-27 00:07:50 +0200

    Update script.js

--- client/script.js
+++ client/script.js
@@ -1,9 +1,28 @@
 //edit your code 
-fetch('http://localhost:3000/user',{
+fetch('http://localhost:3000/user?name="Reham"',{
     method:'GET',
     headers: {
         'Content-Type': 'application/json',
       },
+      body:{
+        name: 'Hello Reham',
+        }
+
+}).then(res=>res.json()).then(val=>console.log(val))
+.catch(err=>{
+    console.log(err);
+})
+
+
+fetch('http://localhost:3000/user',{
+    method:'post',
+    headers: {
+        'Content-Type' : 'application/json' ,
+          },
+      body:{
+        message: 'Hello ',
+        name: 'Reham ',
+        }
 }).then(res=>res.json()).then(val=>console.log(val))
 .catch(err=>{
     console.log(err);
